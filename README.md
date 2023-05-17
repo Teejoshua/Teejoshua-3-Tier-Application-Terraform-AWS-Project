deaee5acc7898cbf5a825536b1f65f1e57dec334
teejoshua <teejoshua01@gmail.com>
2023-05-17 07:19:03 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Terraform to provision AWS resources
+
+## Provisioning a three-tier acrchitecture resources using terraform
+
+- The diagram below shows resources that should be provisioned using terraform
+
+![This](/image/Challenge1.jpeg)
+
+### terraform commands
+
+- `terraform init`
+- `terraform plan`
+- `terraform apply`
+
+### backend and frontend deployments
+
+- Deploying a BookApp application created using NodeJS and ReactJS \\
+  deployment is done using docker for frontend and backend
---
+++ backenddata.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Install docker
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install -y docker-ce
+usermod -aG docker ubuntu
+sudo docker pull marcrine/bookappbackend
+sudo docker run -d -p 5000:5000 marcrine/bookappbackend
